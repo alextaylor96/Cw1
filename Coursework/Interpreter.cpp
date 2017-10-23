2fbd2c1ffb05d2688939bce1b3026f2b3c0a4e4f
b4031639 <b4031639@TERRAN01>
2017-10-23 15:34:39 +0100

    interpreter help needed

--- Coursework/Interpreter.cpp
+++ Coursework/Interpreter.cpp
@@ -32,12 +32,23 @@ result Interpreter::interpretResult(Prisoner& prisoner)
 				return BETRAY;
 			}
 		}
-		
+		//get help here
 		if (prisoner.getStrategy().getTokens().at(i).at(1).getValue() == "IF") {
+			int left;
+			int right;
 
+			if (prisoner.getStrategy().getTokens().at(i).at(2).getType() == INTEGER) {
+				left = std::stoi(prisoner.getStrategy().getTokens().at(i).at(2).getValue());
+			}
 
+			if (prisoner.getStrategy().getTokens().at(i).at(2).getType() == CHARVARIABLE) {
+				left = std::stoi(prisoner.getStrategy().getTokens().at(i).at(2).getValue());
+			}
 
-
+			string s = prisoner.getStrategy().getTokens().at(i).at(2).getValue();
+									
+			if(s == "")
+			
 		}
 
 	}
